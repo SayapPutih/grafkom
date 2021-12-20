cded64a9e9d258bd42171bece92b33ec7fa9974d
Ezza Boby Muhamad <73987189+SayapPutih@users.noreply.github.com>
2021-12-20 12:27:57 +0700

    Update grafkom.py

--- grafkom.py
+++ grafkom.py
@@ -9,7 +9,7 @@ field_width, field_height = 50, 50                     # internal resolution
 snake = [(20, 20)] # snake list of (x, y) positions
 snake_dir = (1, 0) # snake movement direction
 interval = 200 # update interval in milliseconds
-food = []
+food = [] # food list of type (x, y)
 
 def refresh2d_custom(width, height, internal_width, internal_height):
     glViewport(0, 0, width, height)
@@ -50,45 +50,36 @@ def draw():                                            # draw is called all the 
    
     glutSwapBuffers()                                  # important for double bufferin
 
-def update(value):
-    # TODO update things...
-   
-    glutTimerFunc(interval, update, 0) # trigger next update
 
 def vec_add(p1, p2):
     (x1, y1), (x2, y2)=p1, p2
     return (x1 + x2, y1+y2)
-
-def update(value):
-    snake.insert(0, vec_add(snake[0], snake_dir))     
-    snake.pop()                                        
-   
-    glutTimerFunc(interval, update, 0)                
+new_pos = vec_add(snake[0], snake_dir)           
 
 def controller(key, x, y):
+    global snake_dir
+    
     if key == GLUT_KEY_UP:
-        print('atas')
+        snake_dir = (0, 1)
     if key == GLUT_KEY_DOWN:
-        #bola_gerak_vertikal(-1)
-        print("bawah")
+        snake_dir = (0, -1)
     if key == GLUT_KEY_LEFT:
-        #bola_gerak_horizontal(-1)
-        print("kiri")
+        snake_dir = (-1, 0)
     if key == GLUT_KEY_RIGHT:
-        #bola_gerak_horizontal(1)
-        print("kanan")
-
-
-
-def draw_food():
-    glColor3f(0.5, 0.5, 1.0)  
-    for x, y in food:         
-        draw_rect(x, y, 1, 1)
+        snake_dir = (1, 0)
+        
 
 def update(value):  
     # move snake      
     snake.insert(0, vec_add(snake[0], snake_dir))      
     snake.pop()                                        
+   
+# let the snake eat the food
+    (hx, hy) = snake[0]          # get the snake's head x and y position
+    for x, y in food:            # go through the food list
+        if hx == x and hy == y:  # is the head where the food is?
+            snake.append((x, y)) # make the snake longer
+            food.remove((x, y))  # remove the food
    
     # spawn food
     r = randint(0, 20)                                
@@ -98,6 +89,10 @@ def update(value):
    
     glutTimerFunc(interval, update, 0)                 
 
+def draw_food():
+    glColor3f(0.5, 0.5, 1.0)  # set color to blue
+    for x, y in food:         # go through each (x, y) entry
+        draw_rect(x, y, 1, 1) # draw it at (x, y) with width=1 and height=1
 
 def draw():                                            
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) 
@@ -120,9 +115,9 @@ glutInit()
 glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
 glutInitWindowSize(width, height)                     
 glutInitWindowPosition(0, 0)                           
-glutCreateWindow("noobtuts.com")              
+glutCreateWindow("Hungry Snake")              
 glutDisplayFunc(draw)                                 
 glutIdleFunc(draw)                                    
-glutTimerFunc(interval, update, 0)                        
+glutTimerFunc(interval, update, 0)                       
 glutSpecialFunc(controller)                       
 glutMainLoop()
